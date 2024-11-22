cc58963c92ddb79f82b6dd109dceebb925203d0e
Ivan Leshchenko <ivan.leshchenko@rxdefine.com>
2024-11-22 18:44:23 +0200

    feat: material ui added, login page added

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <React.StrictMode>
+      <CssBaseline />
       <App />
     </React.StrictMode>,
   );
--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,7 +1,33 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 export const LoginPage = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 2,
+          padding: 8,
+          border: '1px solid #ccc',
+          borderRadius: 1,
+        }}
+      >
+        <Typography variant="h3">Login MOGUR</Typography>
+        <TextField fullWidth label="Login" variant="standard" />
+        <TextField fullWidth label="Password" variant="standard" />
+        <Button variant="contained">Login</Button>
+      </Box>
+    </Box>
   );
 };
